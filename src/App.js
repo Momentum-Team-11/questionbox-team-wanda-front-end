876bb706c9e6bb0ea91bd9ef5a7fd7fc6f56e968
KarenElaine Stewart <karenelaine.stewart@yahoo.com>
2022-04-09 17:51:49 -0400

    functionality for asking and submiting a question works and new question displays on preview page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import Question from './components/Question'
+import Question from './Components/Question'
 
 const baseURL = 'https://team11-questionbox.herokuapp.com/api'
+const loggedInUser = 'Token 392dbed88909b8014eb15a806220691b2015f316'
 
 const App = () => {
   const [questions, setQuestions] = useState([])
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
 
   useEffect(() => {
     const getQuestions = async () => { await axios.get(baseURL + '/questions').then(res => setQuestions(res.data)) }
     getQuestions()
   }, [])
 
+  const submitQuestion = async () => {
+    return await axios.post(baseURL + '/questions/', {
+      "title": title,
+      "description": description
+    }, {
+      headers: {
+        'Authorization': loggedInUser,
+      }
+    })
+  }
+
+
+
+//handleChange function takes event as param and sets the input
+  const handleChange = (event) => {
+
+    if (event.target.name === 'title') {
+      console.log('title input changed!')
+      setTitle(event.target.value)
+    }
+
+    if (event.target.name === 'description') {
+      console.log('description input changed!')
+      setDescription(event.target.value)
+    }
+  }
+
   return (
     <div>
       <div>
-        <label htmlFor="ask">Ask a question</label>
-        <input placeholder="Just ask!" type="text" name="ask" />
+        <label htmlFor="title">Title</label>
+        <input
+          placeholder="Title"
+          type="text"
+          name="title"
+          value={title}
+          onChange={(e) => handleChange(e)} />
       </div>
+
+      <div>
+        <label htmlFor="description">Description</label>
+        <input
+          placeholder="Description"
+          type="text"
+          name="description"
+          value={description}
+          onChange={(e) => handleChange(e)} />
+      </div>
+      <button onClick={submitQuestion}>Submit</button>
+
+      {/* list of questions */}
       {questions.map((question) => {
         return (
           <Question key={question.pk} question={question} />
@@ -28,6 +76,8 @@ const App = () => {
 }
 
 export default App;
+
+
 
 
 
